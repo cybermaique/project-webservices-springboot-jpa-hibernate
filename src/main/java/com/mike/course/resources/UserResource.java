3d41af54b62c0c42a786c2669e716def1687aca9
cybermaique <maiqued.18@gmail.com>
2022-01-02 23:58:08 -0300

    User entity and resource

---
+++ src/main/java/com/mike/course/resources/UserResource.java
@@ -0,0 +1,16 @@
+package com.mike.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mike.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maique", "maiqued.18@gmail.com", "99999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
