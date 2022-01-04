141b3765c2c5ad934ffefa5cd38cd10eb6998c9c
cybermaique <maiqued.18@gmail.com>
2022-01-03 23:56:07 -0300

    h2 config

--- src/main/java/com/mike/course/resources/UserResource.java
+++ src/main/java/com/mike/course/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.mike.course.resources;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,8 @@ import com.mike.course.entities.User;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+	
+	@GetMapping //responds to get request
 	public ResponseEntity<User> findAll() {
 		User u = new User(1L, "Maique", "maiqued.18@gmail.com", "99999", "12345");
 		return ResponseEntity.ok().body(u);
